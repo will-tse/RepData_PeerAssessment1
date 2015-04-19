2c308e7fd74a4fc0293ddba697de5226f0d8cda2
William Tse <william_tse@yahoo.com>
2015-04-19 15:12:57 -0400

    Using Knit2html
    Using R to make md and html files

---
+++ make_md_and_html.r
@@ -0,0 +1,5 @@
+library(knitr);
+
+setwd('D:/Coursera/Johns Hopkins/Data Science/05 - Reproducible Research/GitHub/RepData_PeerAssessment1/');
+knit('PA1_template.Rmd');
+knit2html('PA1_template.Rmd');
